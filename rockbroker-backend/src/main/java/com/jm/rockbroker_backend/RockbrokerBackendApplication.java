2386526b4e5aa8885e28ce9d0d3cfa00c1bfb397
Joshua Menezes <joshua.menezes@outlook.com>
2024-09-12 15:59:52 -0400

    Initialize spring project

---
+++ rockbroker-backend/src/main/java/com/jm/rockbroker_backend/RockbrokerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jm.rockbroker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockbrokerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockbrokerBackendApplication.class, args);
+	}
+
+}
---
+++ rockbroker-backend/src/test/java/com/jm/rockbroker_backend/RockbrokerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jm.rockbroker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockbrokerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
